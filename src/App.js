632e8287f8be9602b00dc74dc2cee56b09dd5e0f
adikso8 <goku3@onet.pl>
2024-02-17 18:07:49 +0100

    Added CSS classes and a HTML template

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className="header">
+        <h2>React TODO LIST</h2>
+        <button >New Post</button>
+      </div>
+      <div className='list'>
+        <div className='post new'>
+          <p className='date'>17.02.2024 15:51</p>
+          <p className='text'>Three MAGNEMITE are linked by a strong magnetic force. Earaches will occur if you get too close.</p>
+        </div>
+
+        <div className='post pending'>
+          <p className='date'>17.02.2024 15:54</p>
+          <p className='text'>SEADRA generates whirlpools by spinning its body. The whirlpools are strong enough to swallow even fishing boats. This Pokémon weakens prey with these currents, then swallows it whole. </p>
+        </div>
+
+        <div className='post finished'>
+          <p className='date'>17.02.2024 15:56</p>
+          <p className='text'>RHYDON's horn can crush even uncut diamonds. One sweeping blow of its tail can topple a building. This Pokémon's hide is extremely tough. Even direct cannon hits don't leave a scratch.</p>
+        </div>
+
+      </div>
     </div>
   );
 }
